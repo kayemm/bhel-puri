ace070670cede84078ad31e7700e6f28a0fa9c30
kayemm <krishnamoorthy.b@gmail.com>
2013-11-17 21:08:29 +0530

    Added Images

--- mapred-lab1/MapReduce.py
+++ mapred-lab1/MapReduce.py
@@ -1,5 +1,12 @@
+# Original Author : Course staff, "Introduction to data science", coursera.org
+# Modified By     : Krishnamoorthy B
+# Modifications   : 1. Added support for the following file formats
+#                          a. Comma Separated Values
+#                          b. XML files containing the StackExchange dump.
+#                          c. Plain text files. 
 import json
 import csv
+import xml.etree.ElementTree as ET
 
 class MapReduce:
     def __init__(self):
@@ -12,16 +19,33 @@ class MapReduce:
 
     def emit(self, value):
         self.result.append(value) 
-
-    def execute(self, data, mapper, reducer,fileFormat):
+    # data - Name of the Input File
+    # 
+    if (fileFormat <> "SOXML"):
+        data = open(fileName)
+        
+    def execute(self, fileName, mapper, reducer,fileFormat):
         if (fileFormat == "JSON"):
             for line in data:
                 record = json.loads(line)
                 mapper(record)
+        
         if (fileFormat == "CSV"):
             csvReader = csv.reader(data,delimiter=',')
             for line in csvReader:
                 mapper(line)
+        
+        # SOXML is used to identify XML file dumps of StackExchange datasets.
+        # In all StackExchange XML files, the main data is stored as attributes
+        # of the xml element 'row'. We extract the attributes of the element as
+        # a dictionary and pass that to the mapper. The mapper is responsible for
+        # extracting the correct attributes from the dictionary.
+        if (fileFormat == "SOXML"):
+            xmlTree = ET.parse(fileName)
+            treeRoot = xmlTree.getRoot()
+            for child in root:
+                if (child.tag == "row"):
+                    mapper(child.attrib)
         
         for key in self.intermediate:
             reducer(key, self.intermediate[key])
